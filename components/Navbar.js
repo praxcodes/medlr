f68662252de1390ba00396cce3571f608496ab18
praxcodes <157406959+praxcodes@users.noreply.github.com>
2024-06-03 19:59:10 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
 import styles from '../styles/Navbar.module.css';
+import Link from 'next/link';
 
 const Navbar = () => {
   const [search, setSearch] = useState('');
